2b24ba3d7f33b9b33d9cc71ebeed983540bdb2cb
Peng Zheng <zhengp@uw.edu>
2022-04-06 11:38:14 -0700

    Add optim module (#2)
    * Add the interior point solver
    
    * Add optimization results
    
    * Correct the iterations in the ipsolve
    
    * Add objective function into the solver
    
    * Add test to ipsolver
    
    * Add newton's solver
    
    * Add test for newton's solver
    
    * Add the docstrings
    
    * Update documentation of the solvers

---
+++ tests/optim/test_ntsolver.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pytest
+from msca.linalg.matrix import asmatrix
+from msca.optim.ntsolver import NTSolver
+
+np.random.seed(123)
+mat = asmatrix(np.eye(5))
+vec = np.random.randn(5)
+
+
+def objective(x):
+    r = vec - mat.dot(x)
+    return 0.5*(r**2).sum()
+
+
+def gradient(x):
+    r = vec - mat.dot(x)
+    return -mat.T.dot(r)
+
+
+def hessian(x):
+    return mat.T.dot(mat)
+
+
+def test_ntsolver():
+    solver = NTSolver(
+        objective,
+        gradient,
+        hessian,
+    )
+    result = solver.minimize(
+        x0=np.zeros(5), gtol=1e-10, xtol=0.0,
+    )
+    assert result.success
+    assert np.allclose(result.x, vec)
